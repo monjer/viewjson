566f8b13478eaa2a73a07b735c39b3f27d9a562a
monjer <hmj030986@gmail.com>
2024-12-07 11:42:52 +0800

    fix: change  domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,42 +3,42 @@ import type { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://viewjson.com',
+      url: 'https://viewjson.online',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     }, {
-      url: 'https://viewjson.com/convert/json-to-xml',
+      url: 'https://viewjson.online/convert/json-to-xml',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.8,
     }, {
-      url: 'https://viewjson.com/convert/json-to-yaml',
+      url: 'https://viewjson.online/convert/json-to-yaml',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.8,
     }, {
-      url: 'https://viewjson.com/convert/json-to-cvs',
+      url: 'https://viewjson.online/convert/json-to-cvs',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.8,
     }, {
-      url: 'https://viewjson.com/convert/json-to-base64',
+      url: 'https://viewjson.online/convert/json-to-base64',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.8,
     }, {
-      url: 'https://viewjson.com/convert/json-to-html',
+      url: 'https://viewjson.online/convert/json-to-html',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.8,
     }, {
-      url: 'https://viewjson.com/diff',
+      url: 'https://viewjson.online/diff',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.8,
     }, {
-      url: 'https://viewjson.com/validate',
+      url: 'https://viewjson.online/validate',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.8,
