061caae812ef16d0c1a9e86fa04c21669e9fbba4
monjer <hmj030986@gmail.com>
2025-06-18 21:31:21 +0800

    fix: some button style change

--- app/format/Content/index.tsx
+++ app/format/Content/index.tsx
@@ -231,7 +231,7 @@ function Main() {
             <Divider vertical />
             <Button onClick={onCleanBtnClick} title="clean json string"><Eraser size={14} className="mr-1" />Clean</Button>
           </Flex>
-          <Button type="text" onClick={onExpandBtnClick}>
+          <Button type="text" onClick={onExpandBtnClick} className="px-1">
             {expand ? <Shrink size={16} /> : <Expand size={16} />}
           </Button>
         </Flex>
