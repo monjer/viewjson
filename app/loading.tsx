b4ad92e76aeb966c2a1edb01ed9728c5ac39ba63
monjer <hmj030986@gmail.com>
2025-09-09 22:45:23 +0800

    fix: to https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Spinner from '@/components/Spinner';
 
 export default function Loading() {
 
   return (
     <div className='text-center p-10'>
-      <Spinner variant="dots" />
+
     </div>
   );
 }
